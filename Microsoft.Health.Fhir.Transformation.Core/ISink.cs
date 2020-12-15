f5d4ec359e7b5dc293a2848d26933a60fa83e3c4
skwwt <tong.wu@live.com>
2020-12-15 11:29:05 +0800

    add contributing md

--- Microsoft.Health.Fhir.Transformation.Core/ISink.cs
+++ Microsoft.Health.Fhir.Transformation.Core/ISink.cs
@@ -1,7 +1,5 @@
-﻿// -------------------------------------------------------------------------------------------------
-// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
-// -------------------------------------------------------------------------------------------------
 
 using System.Collections.Generic;
 using Task = System.Threading.Tasks.Task;
