6bc5bfe826067a4f441799ed5990fb3f5a797cf2
Yue Fei <yufei@microsoft.com>
2022-01-21 13:58:45 +0800

    Fix tests

--- FhirToDataLake/test/Microsoft.Health.Fhir.Synapse.Core.UnitTests/Jobs/InMemoryJobManagerTests.cs
+++ FhirToDataLake/test/Microsoft.Health.Fhir.Synapse.Core.UnitTests/Jobs/InMemoryJobManagerTests.cs
@@ -205,7 +205,7 @@ namespace Microsoft.Health.Fhir.Synapse.Core.UnitTests.Jobs
 
             var taskExecutor = Substitute.For<ITaskExecutor>();
             taskExecutor.ExecuteAsync(Arg.Any<TaskContext>(), Arg.Any<JobProgressUpdater>(), Arg.Any<CancellationToken>()).Returns(CreateTestTaskResult());
-            var jobExecutor = new JobExecutor(jobStore, taskExecutor, new JobProgressUpdaterFactory(jobStore), Options.Create(schedulerConfig), new NullLogger<JobExecutor>());
+            var jobExecutor = new JobExecutor(taskExecutor, new JobProgressUpdaterFactory(jobStore), Options.Create(schedulerConfig), new NullLogger<JobExecutor>());
 
             return new JobManager(
                 jobStore,
