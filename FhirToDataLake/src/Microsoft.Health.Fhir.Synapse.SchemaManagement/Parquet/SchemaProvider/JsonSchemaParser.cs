202340219dc9f816fb023bdc2d4a05082a027628
QuanWanxx <68055742+QuanWanxx@users.noreply.github.com>
2022-08-08 17:25:59 +0800

    Update the json schema lib

--- FhirToDataLake/src/Microsoft.Health.Fhir.Synapse.SchemaManagement/Parquet/SchemaProvider/JsonSchemaParser.cs
+++ FhirToDataLake/src/Microsoft.Health.Fhir.Synapse.SchemaManagement/Parquet/SchemaProvider/JsonSchemaParser.cs
@@ -5,7 +5,7 @@
 
 using System.Collections.Generic;
 using Microsoft.Health.Fhir.Synapse.SchemaManagement.Exceptions;
-using Newtonsoft.Json.Schema;
+using NJsonSchema;
 
 namespace Microsoft.Health.Fhir.Synapse.SchemaManagement.Parquet.SchemaProvider
 {
@@ -18,14 +18,14 @@ namespace Microsoft.Health.Fhir.Synapse.SchemaManagement.Parquet.SchemaProvider
         /// <param name="resourceType">The resource type.</param>
         /// <param name="jsonSchema">The json schema.</param>
         /// <returns>A FhirParquetSchemaNode instance.</returns>
-        public static FhirParquetSchemaNode ParseJSchema(string resourceType, JSchema jsonSchema)
+        public static FhirParquetSchemaNode ParseJSchema(string resourceType, JsonSchema jsonSchema)
         {
-            if (jsonSchema.Type == null || jsonSchema.Type == JSchemaType.Null)
+            if (jsonSchema.Type == JsonObjectType.Null)
             {
                 throw new GenerateFhirParquetSchemaNodeException(string.Format("The \"{0}\" customized schema have no \"type\" keyword or \"type\" is null.", resourceType));
             }
 
-            if (jsonSchema.Type != JSchemaType.Object)
+            if (jsonSchema.Type != JsonObjectType.Object)
             {
                 throw new GenerateFhirParquetSchemaNodeException(string.Format("The \"{0}\" customized schema type \"{1}\" should be \"object\".", resourceType, jsonSchema.Type));
             }
@@ -45,19 +45,19 @@ namespace Microsoft.Health.Fhir.Synapse.SchemaManagement.Parquet.SchemaProvider
             {
                 fhirPath.Add(property.Key);
 
-                if (property.Value.Type == null || property.Value.Type == JSchemaType.Null)
+                if (property.Value.Type == JsonObjectType.Null)
                 {
                     throw new GenerateFhirParquetSchemaNodeException(string.Format("Property \"{0}\" for \"{1}\" customized schema have no \"type\" keyword or \"type\" is null.", property.Key, resourceType));
                 }
 
-                if (!FhirParquetSchemaConstants.BasicJSchemaTypeMap.ContainsKey(property.Value.Type.Value))
+                if (!FhirParquetSchemaConstants.BasicJSchemaTypeMap.ContainsKey(property.Value.Type))
                 {
-                    throw new GenerateFhirParquetSchemaNodeException(string.Format("Property \"{0}\" type \"{1}\" for \"{2}\" customized schema is not basic type.", property.Key, property.Value.Type.Value, resourceType));
+                    throw new GenerateFhirParquetSchemaNodeException(string.Format("Property \"{0}\" type \"{1}\" for \"{2}\" customized schema is not basic type.", property.Key, property.Value.Type, resourceType));
                 }
 
                 customizedSchemaNode.SubNodes.Add(
                     property.Key,
-                    BuildLeafNode(property.Key, FhirParquetSchemaConstants.BasicJSchemaTypeMap[property.Value.Type.Value], 1, fhirPath));
+                    BuildLeafNode(property.Key, FhirParquetSchemaConstants.BasicJSchemaTypeMap[property.Value.Type], 1, fhirPath));
 
                 fhirPath.RemoveAt(fhirPath.Count - 1);
             }
