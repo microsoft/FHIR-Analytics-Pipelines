b9b2e6a878a691884cd45c51bb9c3aa155babf6e
songrui <sowu@microsoft.com>
2022-08-26 15:13:58 +0800

    add log metrics

---
+++ FhirToDataLake/src/Microsoft.Health.Fhir.Synapse.Common/Metrics/DimensionNames.cs
@@ -0,0 +1,16 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+namespace Microsoft.Health.Fhir.Synapse.Common.Metrics
+{
+    public static class DimensionNames
+    {
+        public static string Name => nameof(DimensionNames.Name);
+
+        public static string Category => nameof(DimensionNames.Category);
+
+        public static string IsInternal => nameof(DimensionNames.IsInternal);
+    }
+}
---
+++ FhirToDataLake/src/Microsoft.Health.Fhir.Synapse.Common/Metrics/TotalErrorMetrics.cs
@@ -0,0 +1,22 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+
+namespace Microsoft.Health.Fhir.Synapse.Common.Metrics
+{
+    public class TotalErrorMetrics : ExternalMetrics
+    {
+        public TotalErrorMetrics()
+            : base("TotalError", MetricsCategories.Errors, new Dictionary<string, object>
+            {
+                { DimensionNames.Name, "TotalError" },
+                { DimensionNames.Category, MetricsCategories.Errors },
+                { DimensionNames.IsInternal, false },
+            })
+        {
+        }
+    }
+}
