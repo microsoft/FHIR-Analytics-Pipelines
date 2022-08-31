270dc5a8443f7bc8e1e9d5b38a66de515d4a3ed3
sowu880 <57981365+sowu880@users.noreply.github.com>
2022-08-31 16:27:19 +0800

    log metrics (#157)
    * add log metrics
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update

---
+++ FhirToDataLake/src/Microsoft.Health.Fhir.Synapse.Common/Metrics/DiagnosticMetrics.cs
@@ -0,0 +1,17 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+
+namespace Microsoft.Health.Fhir.Synapse.Common.Metrics
+{
+    public class DiagnosticMetrics : Metrics
+    {
+        protected DiagnosticMetrics(string name, MetricsCategories category, IDictionary<string, object> dimensions)
+            : base(name, category, dimensions)
+        {
+        }
+    }
+}
