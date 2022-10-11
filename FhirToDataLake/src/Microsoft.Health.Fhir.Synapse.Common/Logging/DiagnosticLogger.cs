afe7aff1214bd59dd636b3bb6b4763c8041d9678
songrui <sowu@microsoft.com>
2022-10-11 15:47:42 +0800

    update

--- FhirToDataLake/src/Microsoft.Health.Fhir.Synapse.Common/Logging/DiagnosticLogger.cs
+++ FhirToDataLake/src/Microsoft.Health.Fhir.Synapse.Common/Logging/DiagnosticLogger.cs
@@ -3,23 +3,25 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
+using System;
+
 namespace Microsoft.Health.Fhir.Synapse.Common.Logging
 {
     public class DiagnosticLogger : IDiagnosticLogger
     {
         public void LogInformation(string message)
         {
-
+            Console.WriteLine("Info: " + message);
         }
 
         public void LogWarning(string message)
         {
-
+            Console.WriteLine("Warning: " + message);
         }
 
         public void LogError(string errorMessage)
         {
-
+            Console.WriteLine("Error: " + errorMessage);
         }
     }
 }
