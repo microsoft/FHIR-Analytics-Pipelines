d8fe00f8afd090706772f3419818e30c005cda0c
Yanhui Hong <yanhon@microsoft.com>
2022-11-25 16:25:10 +0800

    address comments

---
+++ FhirToDataLake/src/Microsoft.Health.Fhir.Synapse.Core/Jobs/Models/DicomToDataLakeProcessingJobResult.cs
@@ -0,0 +1,48 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.Health.Fhir.Synapse.Core.Jobs.Models
+{
+    public class DicomToDataLakeProcessingJobResult
+    {
+        /// <summary>
+        /// Processing job start time
+        /// </summary>
+        public DateTimeOffset ProcessingStartTime { get; set; } = DateTimeOffset.UtcNow;
+
+        /// <summary>
+        /// Processing job complete time
+        /// </summary>
+        public DateTimeOffset? ProcessingCompleteTime { get; set; }
+
+        /// <summary>
+        /// Search count for each resource type.
+        /// </summary>
+        public Dictionary<string, int> SearchCount { get; set; } = new Dictionary<string, int>();
+
+        /// <summary>
+        /// Skipped count for each schema type.
+        /// </summary>
+        public Dictionary<string, int> SkippedCount { get; set; } = new Dictionary<string, int>();
+
+        /// <summary>
+        /// Processed count for each schema type.
+        /// </summary>
+        public Dictionary<string, int> ProcessedCount { get; set; } = new Dictionary<string, int>();
+
+        /// <summary>
+        /// Output resource count in total.
+        /// </summary>
+        public int ProcessedCountInTotal { get; set; } = 0;
+
+        /// <summary>
+        /// Data size for output parquet data in bytes.
+        /// </summary>
+        public long ProcessedDataSizeInTotal { get; set; } = 0;
+    }
+}
