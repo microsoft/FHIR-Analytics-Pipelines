3de0b484f37c88b17af47546b2fd3ef06fe0b44e
Yue Fei <yufei@microsoft.com>
2022-11-30 12:20:45 +0800

    Update manifest in external dependency checker

--- FhirToDataLake/src/Microsoft.Health.Fhir.Synapse.HealthCheck/Checkers/AzureContainerRegistryHealthChecker.cs
+++ FhirToDataLake/src/Microsoft.Health.Fhir.Synapse.HealthCheck/Checkers/AzureContainerRegistryHealthChecker.cs
@@ -10,6 +10,7 @@ using EnsureThat;
 using Microsoft.Azure.ContainerRegistry;
 using Microsoft.Extensions.Logging;
 using Microsoft.Health.Fhir.Synapse.Common.Logging;
+using Microsoft.Health.Fhir.Synapse.Core.ContainerRegistry;
 using Microsoft.Health.Fhir.Synapse.HealthCheck.Models;
 using Microsoft.Health.Fhir.Synapse.SchemaManagement.ContainerRegistry;
 using Microsoft.Health.Fhir.TemplateManagement.Client;
@@ -19,7 +20,6 @@ namespace Microsoft.Health.Fhir.Synapse.HealthCheck.Checkers
 {
     public class AzureContainerRegistryHealthChecker : BaseHealthChecker
     {
-        private const string ManifestMediatype = "application/vnd.oci.image.manifest.v1+json,application/vnd.docker.distribution.manifest.v2+json";
         private readonly string _imageReference;
         private readonly IContainerRegistryTokenProvider _containerRegistryTokenProvider;
         private readonly string _name;
@@ -49,7 +49,7 @@ namespace Microsoft.Health.Fhir.Synapse.HealthCheck.Checkers
                 var acrClient = new AzureContainerRegistryClient(imageInfo.Registry, new AcrClientCredentials(accessToken));
 
                 // Ensure we can read from acr.
-                await acrClient.Manifests.GetAsync(imageInfo.ImageName, imageInfo.Label, ManifestMediatype, cancellationToken);
+                await acrClient.Manifests.GetAsync(imageInfo.ImageName, imageInfo.Label, OciArtifactConstants.AcceptedManifestTypes, cancellationToken);
             }
             catch (Exception e)
             {
