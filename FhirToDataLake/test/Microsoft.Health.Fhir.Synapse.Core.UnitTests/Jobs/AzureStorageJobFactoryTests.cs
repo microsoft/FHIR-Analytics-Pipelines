15e6c273cc4f2419235ebedb441db5c6dd5f0366
Yanhui Hong <yanhon@microsoft.com>
2023-01-12 23:08:31 +0800

    add unit tests

--- FhirToDataLake/test/Microsoft.Health.Fhir.Synapse.Core.UnitTests/Jobs/AzureStorageJobFactoryTests.cs
+++ FhirToDataLake/test/Microsoft.Health.Fhir.Synapse.Core.UnitTests/Jobs/AzureStorageJobFactoryTests.cs
@@ -9,26 +9,88 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Options;
 using Microsoft.Health.Fhir.Synapse.Common.Configurations;
+using Microsoft.Health.Fhir.Synapse.Common.Logging;
+using Microsoft.Health.Fhir.Synapse.Common.Metrics;
 using Microsoft.Health.Fhir.Synapse.Common.Models.Jobs;
 using Microsoft.Health.Fhir.Synapse.Core.DataFilter;
+using Microsoft.Health.Fhir.Synapse.Core.DataProcessor;
 using Microsoft.Health.Fhir.Synapse.Core.Jobs;
 using Microsoft.Health.Fhir.Synapse.Core.Jobs.Models;
-using Microsoft.Health.Fhir.Synapse.DataWriter.Azure;
+using Microsoft.Health.Fhir.Synapse.DataClient;
+using Microsoft.Health.Fhir.Synapse.DataWriter;
+using Microsoft.Health.Fhir.Synapse.SchemaManagement;
+using Microsoft.Health.Fhir.Synapse.SchemaManagement.Parquet;
 using Microsoft.Health.JobManagement;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using NSubstitute;
 using NSubstitute.ReturnsExtensions;
 using Xunit;
 
 namespace Microsoft.Health.Fhir.Synapse.Core.UnitTests.Jobs
 {
-    public class JobHostingTests
+    public class AzureStorageJobFactoryTests
     {
         private const string TestWorkerName = "test-worker";
 
         [Fact]
-        public async Task GivenUnsupportedJobVersion_WhenCreateJob_ThenShouldSkipAndRevisualableInQueue()
+        public async Task GivenUnsupportedJobVersion_WhenCreateJob_ThenShouldReturnNull()
+        {
+            var queueClient = new MockQueueClient<FhirToDataLakeAzureStorageJobInfo>();
+
+            var jobFactory = new AzureStorageJobFactory(
+                queueClient,
+                Substitute.For<IApiDataClient>(),
+                Substitute.For<IDataWriter>(),
+                Substitute.For<IGroupMemberExtractor>(),
+                Substitute.For<IColumnDataProcessor>(),
+                Substitute.For<ISchemaManager<ParquetSchemaNode>>(),
+                Substitute.For<IFilterManager>(),
+                Substitute.For<IMetadataStore>(),
+                Options.Create(new JobConfiguration()),
+                new MetricsLogger(new NullLogger<MetricsLogger>()),
+                new DiagnosticLogger(),
+                new NullLoggerFactory());
+
+            // create job with unsupported job version
+            var orchestratorDefinition = new FhirToDataLakeOrchestratorJobInputData
+            {
+                JobType = JobType.Orchestrator,
+                TriggerSequenceId = 1,
+                Since = DateTimeOffset.MinValue,
+                DataStartTime = DateTimeOffset.MinValue,
+                DataEndTime = DateTimeOffset.UtcNow,
+            };
+
+            var jobject = JObject.FromObject(orchestratorDefinition);
+
+            jobject[JobVersionManager.JobVersionKey] = "UnsupportedJobVersion";
+
+            // enqueue job
+            List<JobInfo> jobInfoList = (await queueClient.EnqueueAsync(
+                (byte)QueueType.FhirToDataLake,
+                new[] { JsonConvert.SerializeObject(jobject) },
+                0,
+                false,
+                false,
+                CancellationToken.None)).ToList();
+
+            Assert.Single(jobInfoList);
+
+            // dequeue job
+            var jobInfo = await queueClient.DequeueAsync((byte)QueueType.FhirToDataLake, TestWorkerName, 0, CancellationToken.None);
+
+            Assert.NotNull(jobInfo);
+            var job = jobFactory.Create(jobInfo);
+
+            // the job is null for unsupported job version
+            Assert.Null(job);
+        }
+
+        [Fact]
+        public async Task GivenJobFactoryReturnNull_WhenRunningJobHosting_ThenShouldSkipAndRevisualableInQueue()
         {
             var queueClient = new MockQueueClient<FhirToDataLakeAzureStorageJobInfo>();
 
@@ -72,6 +134,9 @@ namespace Microsoft.Health.Fhir.Synapse.Core.UnitTests.Jobs
 
             Assert.NotNull(jobInfo);
             Assert.Equal(JobStatus.Running, jobInfo.Status);
+
+            cancellationTokenSource.Cancel();
+            await task;
         }
     }
 }
