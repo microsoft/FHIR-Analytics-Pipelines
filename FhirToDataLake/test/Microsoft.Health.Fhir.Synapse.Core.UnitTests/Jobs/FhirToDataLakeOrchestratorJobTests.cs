e3c28e9c0c51a3b195dae3c945d5e5656c5702c7
Songrui Wu <sowu@microsoft.com>
2023-01-30 11:18:18 +0800

    fix

--- FhirToDataLake/test/Microsoft.Health.Fhir.Synapse.Core.UnitTests/Jobs/FhirToDataLakeOrchestratorJobTests.cs
+++ FhirToDataLake/test/Microsoft.Health.Fhir.Synapse.Core.UnitTests/Jobs/FhirToDataLakeOrchestratorJobTests.cs
@@ -383,6 +383,7 @@ namespace Microsoft.Health.Fhir.Synapse.Core.UnitTests.Jobs
                 NumberOfPatientsPerProcessingJob = 1,
             };
 
+            var startExecuteTime = DateTimeOffset.Now;
             string resultString = await job.ExecuteAsync(progress, CancellationToken.None);
             var result = JsonConvert.DeserializeObject<FhirToDataLakeOrchestratorJobResult>(resultString);
 
@@ -410,6 +411,7 @@ namespace Microsoft.Health.Fhir.Synapse.Core.UnitTests.Jobs
             Assert.Equal(3, metricsLogger.MetricsDic.Count);
             Assert.Equal(inputFileCount, metricsLogger.MetricsDic[MetricNames.SuccessfulResourceCountMetric]);
             Assert.Equal(inputFileCount * 1000L * TBValue, metricsLogger.MetricsDic[MetricNames.SuccessfulDataSizeMetric]);
+            Assert.True((startExecuteTime - TestEndTime).TotalSeconds <= metricsLogger.MetricsDic[MetricNames.ResourceLatencyMetric]);
             Assert.True((DateTimeOffset.UtcNow - TestEndTime).TotalSeconds >= metricsLogger.MetricsDic[MetricNames.ResourceLatencyMetric]);
             Assert.Equal(inputFileCount, queueClient.JobInfos.Count - 1);
 
